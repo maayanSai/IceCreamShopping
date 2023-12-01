883e2c56bf0ec751a055919581f2ffd4a8550004
maayan saidov <maayan18403@gmail.com>
2023-12-01 09:51:00 +0200

    Added a button to the order view, which reveals the payment

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using IceCreamsShopping.ApiService; // Add this import statement
 
-var builder = WebApplication.CreateBulder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
